95d5fc3b95232120ded37ffcfa1cec49cb7b1bcc
hithisismiaa <day082915@gmail.com>
2026-04-20 20:30:37 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,8 @@
 
+export default function Footer() {
+  return (
+    <footer className="py-20 text-center text-gray-500 text-sm">
+      © 2026 Kim Dasom. Creative Director Portfolio
+    </footer>
+  );
+}
