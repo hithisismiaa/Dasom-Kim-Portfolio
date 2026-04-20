c35f0580c7408ca0eab343d0e250af7ea7acaae9
hithisismiaa <day082915@gmail.com>
2026-04-20 20:28:45 +0900

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,17 @@
 
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-6 text-sm text-gray-300">
+      <Link href="/" className="font-bold text-white">
+        KIM DASOM
+      </Link>
+
+      <div className="flex gap-6">
+        <Link href="/">Home</Link>
+        <Link href="/projects">Projects</Link>
+      </div>
+    </nav>
+  );
+}
